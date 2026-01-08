754df9ffd879f32785dc86640c896d24c73368a0
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-08 08:09:34 +0000

    搭接面的路由逻辑

---
+++ routers/__init__.py
@@ -0,0 +1,11 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-07 11:16:29
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-07 11:16:30
+@FilePath     : __init__.py
+@Description  :
+'''
+
+from .dc_fuse import dc_router
+from .lap_surf import lap_surf_router
