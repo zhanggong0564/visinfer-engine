0f04db39b953791988fbd67a1567b05086facb2b
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-13 08:35:30 +0000

    master: refactor: 新增模块导入

---
+++ services/plate_screw/__init__.py
@@ -0,0 +1,11 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-13 05:15:44
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-13 06:50:05
+@FilePath     : __init__.py
+@Description  :
+'''
+
+from .yolo import yolo11ONNX
+from .business_logic import PlateScrewJudgeApi
