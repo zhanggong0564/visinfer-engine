11e890c1e97ff61600e61371fd4f5cf8c7c52bce
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-16 08:04:23 +0000

    feat: 新增__init__.py文件，导入IndicatorLightBusinessAPI类

---
+++ services/indicator_light/__init__.py
@@ -0,0 +1,10 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-16 03:29:45
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-16 03:31:21
+@FilePath     : __init__.py
+@Description  :
+'''
+
+from .business_logic import IndicatorLightBusinessAPI
