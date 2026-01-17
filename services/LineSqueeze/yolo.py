011df111ab63e82d7d81897905521e73be021968
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-17 06:34:17 +0000

    master: feat: 1. 添加RoiDet类处理图像边界框检测 2. 修改初始化参数和后处理函数

---
+++ services/LineSqueeze/yolo.py
@@ -0,0 +1,55 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-17 06:31:48
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-17 06:32:53
+@FilePath     : yolo.py
+@Description  :
+'''
+
+from ..base import BaseOnnxInfer, letterbox, scale_boxes, xywhr2xyxyxyxy
+from ..box import non_max_suppression_v8
+from collections import defaultdict
+import numpy as np
+
+
+class RoiDet(BaseOnnxInfer):
+    def __init__(self, model_path, nc, input_model_shape, confThreshold=0.5, nmsThreshold=0.5, providers=None):
+        super().__init__(model_path, input_model_shape, confThreshold, nmsThreshold, providers)
+        self.task = "rect"
+        self.filter_classes = None
+        self.agnostic = False
+        self.nc = nc
+
+    def post_process(self, preds):
+        p = non_max_suppression_v8(
+            preds[0],
+            task=self.task,
+            conf_thres=self.confThreshold,
+            iou_thres=self.nmsThreshold,
+            classes=self.filter_classes,
+            agnostic=self.agnostic,
+            multi_label=False,
+            nc=self.nc,
+        )
+        image_shape = self.image_src_shape[:2]
+        input_shape = self.input_model_shape[2:]
+        res = defaultdict()
+        pred = p[0]
+        pred[:, :4] = scale_boxes(input_shape, pred[:, :4], image_shape, xywh=False)
+        pred = np.concatenate([pred[:, :4], pred[:, -1:], pred[:, 4:6]], axis=-1)
+        bbox = pred[:, :4]  # xywh
+        if self.task == "obb":
+            bbox = xywhr2xyxyxyxy(bbox)
+        # else:
+        #     bbox = xywh2xyxy(bbox)
+        conf = pred[:, -2]
+        clas = pred[:, -1]
+        res["rect"] = bbox.tolist()
+        res["score"] = conf.tolist()
+        res["cls"] = clas.tolist()
+        # 根据分数过滤
+        res["rect"] = [box for box, score in zip(res["rect"], res["score"]) if score >= self.confThreshold]
+        res["score"] = [score for score in res["score"] if score >= self.confThreshold]
+        res["cls"] = [cls for cls, score in zip(res["cls"], res["score"]) if score >= self.confThreshold]
+        return res
--- services/__init__.py
+++ services/__init__.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 06:11:10
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-17 03:11:46
+@LastEditTime : 2026-01-17 06:33:46
 @FilePath     : __init__.py
 @Description  :
 '''
@@ -12,3 +12,4 @@ from .lap_surf import LapSurfJudgeApi
 from .plate_screw import PlateScrewJudgeApi
 from .indicator_light import IndicatorLightBusinessAPI
 from .utils import rotate_points
+from .LineSqueeze import RoiDet
