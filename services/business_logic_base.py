6359e7d41aa1ab50f33c723bdb3d564797573bc7
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-23 08:59:02 +0000

    dev: feat: 新增业务逻辑基类

---
+++ services/business_logic_base.py
@@ -0,0 +1,45 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-23 05:37:39
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-23 08:53:50
+@FilePath     : business_logic_base.py
+@Description  : 业务逻辑基类
+'''
+
+import numpy as np
+from .data_base import InputParamsBusiness, MoMResult, DetectResult
+
+
+class BusinessLogicBase:
+    def __init__(self, settings):
+        self.settings = settings
+        self.detector = None
+        self._initialize(settings)
+
+    def _initialize(self, settings):
+        self._initialize_model(settings)
+
+    def _initialize_model(self, settings):
+        raise NotImplementedError
+
+    def detect(self, InputParamsBusiness: InputParamsBusiness) -> dict:
+        image = InputParamsBusiness.image
+        h, w, _ = image.shape
+        is_registered = InputParamsBusiness.is_registered
+        product_type = InputParamsBusiness.product_type
+        result = self.detector.infer(image)
+        result = self.business_logic_post_process(result, product_type)
+        result = self.result_post_process(result, w, h)
+        if is_registered:
+            result = self.registered_post_process(result)
+        return result
+
+    def business_logic_post_process(self, result: DetectResult, product_type: str) -> MoMResult:
+        raise NotImplementedError
+
+    def registered_post_process(self, result: dict) -> bool:
+        raise NotImplementedError
+
+    def result_post_process(self, result: MoMResult) -> MoMResult:
+        raise NotImplementedError
