7484f2fd3e1353c6461cdee05a11616d9ae70cef
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 10:08:47 +0000

    dev: feat: 1. 修改了导入的YOLO模型，从yolo11ONNX更改为LapSurfDetONNX 2. 优化了检测器初始化方法，增加了异常处理 3. 修改了ROI和检测结果的处理逻辑，添加了异常处理 4. 更新了返回结果的格式，增加了详细列表和错误信息处理

--- services/lap_surf/business_logic.py
+++ services/lap_surf/business_logic.py
@@ -2,33 +2,34 @@
 @Author       : gongzhang4
 @Date         : 2026-01-08 01:41:19
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-08 06:34:55
+@LastEditTime : 2026-01-27 10:08:26
 @FilePath     : business_logic.py
 @Description  :
 '''
 
 from .lap_surf_core import *
-from .yolo import yolo11ONNX
+from .lap_surf_det import LapSurfDetONNX
 from utils import vision_logger
-from ..utils import vis_box_mask
+from ..utils.utils import vis_box_mask
 import cv2
+from ..api import detection_factory
+from ..base import BusinessLogicBase
+from ..data_base import DetectResult, DetectionItem, MoMResult
 
 
-class LapSurfJudgeApi:
-    def __init__(self, model_path, conf_threshold=0.5):
-        self.detector = self.load_model(model_path, conf_threshold)
+@detection_factory.register("lap_surf")
+class LapSurfJudgeApi(BusinessLogicBase):
 
-    def load_model(self, model_path, conf_threshold):
-        return yolo11ONNX(model_path, nc=4, confThreshold=conf_threshold)
+    def _initialize_model(self, settings):
+        try:
+            self.detector = LapSurfDetONNX(
+                settings.lap_surf.model_path, nc=4, confThreshold=settings.lap_surf.confThreshold
+            )
+        except Exception as e:
+            vision_logger.error(f"加载模型失败: {e}")
+            raise e
 
-    def detect(self, im):
-        outputs = self.detector.infer(im)
-        # vis = vis_box_mask(im, outputs)
-        # cv2.imwrite("vis.jpg", vis)
-        results = self.postprocess(outputs)
-        return results
-
-    def postprocess(self, outputs):
+    def business_logic_post_process(self, outputs: DetectResult, product_type: str) -> MoMResult:
         ROIs = []
         # 螺丝
         screws = []
@@ -37,19 +38,20 @@ class LapSurfJudgeApi:
         # 搭接面
         lap_joints = []
         # try:
-        for i, cls in enumerate(outputs["cls"]):
-            x1, y1, x2, y2 = map(int, outputs["rect"][i][:4])
+        # try:
+        for i, cls in enumerate(outputs.class_ids):
+            x1, y1, x2, y2 = map(int, outputs.boxes[i][:4])
             if cls == 0:
-                roi = ROI(BoundingBox(x1, y1, x2, y2, cls, outputs["score"][i], self.detector.image_src_shape))
+                roi = ROI(BoundingBox(x1, y1, x2, y2, cls, outputs.scores[i], self.detector.image_src_shape))
                 ROIs.append(roi)
             elif cls == 2:
-                nut = BoundingBox(x1, y1, x2, y2, cls, outputs["score"][i], self.detector.image_src_shape)
+                nut = BoundingBox(x1, y1, x2, y2, cls, outputs.scores[i], self.detector.image_src_shape)
                 nuts.append(nut)
             elif cls == 3:
-                lap_joint = LapJoint(x1, y1, x2, y2, cls, outputs["score"][i], self.detector.image_src_shape)
+                lap_joint = LapJoint(x1, y1, x2, y2, cls, outputs.scores[i], self.detector.image_src_shape)
                 lap_joints.append(lap_joint)
             else:
-                screw = BoundingBox(x1, y1, x2, y2, cls, outputs["score"][i], self.detector.image_src_shape)
+                screw = BoundingBox(x1, y1, x2, y2, cls, outputs.scores[i], self.detector.image_src_shape)
                 screws.append(screw)
         # 对ROIs进行排序
         ROIs.sort(key=lambda x: (x.bb.x1, x.bb.y1))
@@ -60,17 +62,16 @@ class LapSurfJudgeApi:
         for i, roi in enumerate(rois_info):
             if not roi.is_valid:
                 is_valid = False
-        detailList = []
+        result = MoMResult()
         for roi in rois_info:
             roi_list = roi.to_dict()
-            detailList.extend(roi_list)
-        if len(detailList) == 0:
+            for item in roi_list:
+                result.detailList.append(DetectionItem().from_dict(item))
+        if len(result.detailList) == 0:
             is_valid = False
-            detailList.append({"status": "false", "scene": "", "coordinate": [], "accuracy": 0.0})
-        dict_info = {
-            "detailList": detailList,
-            'status': "true" if is_valid else "false",
-            "message": "success" if is_valid else "failed",
-            "error_msg": "",
-        }
-        return dict_info
+            result.detailList.append(DetectionItem(status=is_valid))
+        result.message = "success" if is_valid else "failed"
+        result.status = is_valid
+        # except Exception as e:
+        #     result = MoMResult(error_msg=str(e))
+        return result
