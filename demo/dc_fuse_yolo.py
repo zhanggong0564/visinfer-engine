99915647688367a61bedd551ea6836d8d893a8e3
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 07:00:02 +0000

    dev: refactor: "-模型路径修改为当前目录 -添加循环推理测试并打印耗时 "

--- demo/dc_fuse_yolo.py
+++ demo/dc_fuse_yolo.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-23 05:20:26
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-23 05:36:18
+@LastEditTime : 2026-01-27 06:57:04
 @FilePath     : dc_fuse_yolo.py
 @Description  : 检测模型
 '''
@@ -16,12 +16,18 @@ import cv2
 
 
 if __name__ == '__main__':
-    model_path = "../weights/dc_fuse_v5.onnx"
+    model_path = "./weights/dc_fuse_v5.onnx"
     confThreshold = 0.6
 
     detector = DCFuseDetector(model_path, confThreshold)
-    image_path = "./data/dc_fuse/lQDPJwNIOLET3G_NC7jND6CwteZlxtGgiyAJOLfvGUpLAA_4000_3000.jpg"
+    image_path = "demo/data/dc_fuse/lQDPJwNIOLET3G_NC7jND6CwteZlxtGgiyAJOLfvGUpLAA_4000_3000.jpg"
     image = cv2.imread(image_path)
-    results = detector.infer(image)
+    import time
+
+    start = time.time()
+    for i in range(100):
+        results = detector.infer(image)
 
     print(results)
+    end = time.time()
+    print(f"推理耗时：{(end - start) / 100}秒")
