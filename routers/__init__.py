aeba89fb8d34e3934d2f21fdaeff51bbc5014459
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 07:01:34 +0000

    dev: refactor: 添加了新的路由注册器模块

--- routers/__init__.py
+++ routers/__init__.py
@@ -2,12 +2,15 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 11:16:29
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-07 11:16:30
+@LastEditTime : 2026-01-27 06:13:56
 @FilePath     : __init__.py
 @Description  :
 '''
 
-from .dc_fuse import dc_router
-from .lap_surf import lap_surf_router
-from .plate import plate_router
-from .indicator import indicator_router
+# from .dc_fuse import dc_router
+# from .lap_surf import lap_surf_router
+# from .plate import plate_router
+# from .indicator import indicator_router
+from .router_registry import RouterRegistry
+
+
