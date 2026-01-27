abfdeed6dee5ac5003103a3796abddbee9265e79
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 10:07:03 +0000

    dev: refactor: 提取关键代码改动： 添加IndicatorLightBusinessAPI模块导入

--- services/__init__.py
+++ services/__init__.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 06:11:10
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-27 05:32:36
+@LastEditTime : 2026-01-27 10:06:53
 @FilePath     : __init__.py
 @Description  :
 '''
@@ -13,5 +13,6 @@
 # from .indicator_light import IndicatorLightBusinessAPI
 # from .LineSqueeze import RoiDet, LineSqueezeRecognition
 from .dc_fuse import DCFuseDetectorAPI
+from .indicator_light import IndicatorLightBusinessAPI
 from .utils import rotate_points
 from .api import detection_factory
