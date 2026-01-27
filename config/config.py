25a84e18375f46e6d92f6cb34354efe3cfcec3c6
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 10:09:47 +0000

    dev: refactor: 添加indicator_light和lap_surf配置类

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-23 06:35:58
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-23 06:43:19
+@LastEditTime : 2026-01-27 09:25:48
 @FilePath     : config.py
 @Description  :
 '''
@@ -10,19 +10,8 @@
 import os
 from pydantic_settings import BaseSettings
 from .dc_fuse_confg import DcFuseConfig
-
-
-class IndicatorLightConfig:
-    class ModelPath:
-        det_model_path: str = "./weights/IndicatorLightDet_v2.onnx"
-        rec_model_path: str = "./weights/IndicatorLightRec_v2.onnx"
-
-    class ConfThreshold:
-        det: float = 0.25
-        rec: float = 0.25
-
-    JSON_PATH: str = "weights/jsons/standard_embeddings.json"
-    SIM_THR: float = 0.7
+from .indicator_light_config import IndicatorLightConfig
+from .lap_surf_conf import LapSufConfig
 
 
 class PlateScrewConfig:
@@ -30,14 +19,9 @@ class PlateScrewConfig:
     confThreshold: float = 0.25
 
 
-class LapSufConfig:
-    model_path: str = "./weights/LapJointSurfRec_v2.onnx"
-    confThreshold: float = 0.4
-
-
 class Settings(BaseSettings):
     API_TITLE: str = "Mobile Vision alg API"
-    API_VERSION: str = "1.0.0"
+    API_VERSION: str = "1.1.2"
 
     HOST: str = "0.0.0.0"
     PORT: int = 3007
@@ -48,6 +32,7 @@ class Settings(BaseSettings):
     lap_surf: LapSufConfig = LapSufConfig()
     plate_screw: PlateScrewConfig = PlateScrewConfig()
     indicator_light: IndicatorLightConfig = IndicatorLightConfig()
+    WORKERS: int = 1
 
     class Config:
         env_file = ".env"
