3a828566929b26beb5bbcad646243fc925d11878
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-27 10:00:45 +0000

    dev: fix: 1. 修改初始化参数并增加任务类型 2. 增加模型类别数nc 3. 优化代码结构

--- services/dc_fuse/dc_fuse_detect.py
+++ services/dc_fuse/dc_fuse_detect.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 06:48:03
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-27 02:20:57
+@LastEditTime : 2026-01-27 07:11:26
 @FilePath     : dc_fuse_detect.py
 @Description  :
 '''
@@ -12,7 +12,7 @@ from ..yolo import YoloOnnxInfer
 
 class DCFuseDetector(YoloOnnxInfer):
     def __init__(self, model_path, confThreshold=0.5, nmsThreshold=0.5, task="det"):
-        super().__init__(model_path, confThreshold, nmsThreshold)
+        super().__init__(model_path, nc=12, confThreshold=confThreshold, nmsThreshold=nmsThreshold, task=task)
         self.id2name = {
             0: "brass_plate_6",
             1: "lower_crossbeam_screw_10",
