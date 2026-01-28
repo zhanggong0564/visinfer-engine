5af41920eb8bf219b601a4774d9f2c204d72f60b
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-28 07:14:17 +0000

    dev: refactor: 1. 添加OCR功能，集成TextRecognition模型识别文本 2. 修改类名为LineSqueezeDetect和LineSqueezePipeline 3. 改进初始化参数，增加task参数 4. 引入id2name字典映射类别名 5. 添加infer方法进行图像检测和OCR识别

--- services/LineSqueeze/yolo.py
+++ services/LineSqueeze/yolo.py
@@ -2,54 +2,34 @@
 @Author       : gongzhang4
 @Date         : 2026-01-17 06:31:48
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-17 06:44:26
+@LastEditTime : 2026-01-28 07:13:33
 @FilePath     : yolo.py
 @Description  :
 '''
 
-from ..base import BaseOnnxInfer, letterbox, scale_boxes, xywhr2xyxyxyxy
-from ..box import non_max_suppression_v8
-from collections import defaultdict
 import numpy as np
+from ..yolo import YoloOnnxInfer
+from paddleocr import TextRecognition
+from ..data_base import OCRResult, DetectResult
 
 
-class RoiDet(BaseOnnxInfer):
-    def __init__(self, model_path, nc, input_model_shape, confThreshold=0.5, nmsThreshold=0.5, providers=None):
-        super().__init__(model_path, confThreshold, nmsThreshold, providers)
-        self.task = "rect"
-        self.filter_classes = None
-        self.agnostic = False
-        self.nc = nc
+class LineSqueezeDetect(YoloOnnxInfer):
+    def __init__(self, model_path, confThreshold=0.5, nmsThreshold=0.5, task="det"):
+        super().__init__(model_path, confThreshold, nmsThreshold, task, nc=2)
+        self.id2name = {0: "fu_line", 1: "dc_line"}
 
-    def post_process(self, preds):
-        p = non_max_suppression_v8(
-            preds[0],
-            task=self.task,
-            conf_thres=self.confThreshold,
-            iou_thres=self.nmsThreshold,
-            classes=self.filter_classes,
-            agnostic=self.agnostic,
-            multi_label=False,
-            nc=self.nc,
-        )
-        image_shape = self.image_src_shape[:2]
-        input_shape = self.input_model_shape[2:]
-        res = defaultdict()
-        pred = p[0]
-        pred[:, :4] = scale_boxes(input_shape, pred[:, :4], image_shape, xywh=False)
-        pred = np.concatenate([pred[:, :4], pred[:, -1:], pred[:, 4:6]], axis=-1)
-        bbox = pred[:, :4]  # xywh
-        if self.task == "obb":
-            bbox = xywhr2xyxyxyxy(bbox)
-        # else:
-        #     bbox = xywh2xyxy(bbox)
-        conf = pred[:, -2]
-        clas = pred[:, -1]
-        res["rect"] = bbox.tolist()
-        res["score"] = conf.tolist()
-        res["cls"] = clas.tolist()
-        # 根据分数过滤
-        res["rect"] = [box for box, score in zip(res["rect"], res["score"]) if score >= self.confThreshold]
-        res["score"] = [score for score in res["score"] if score >= self.confThreshold]
-        res["cls"] = [cls for cls, score in zip(res["cls"], res["score"]) if score >= self.confThreshold]
-        return res
+
+class LineSqueezePipeline:
+    def __init__(self, det_model_path, ocr_model_path, confThreshold=0.5, nmsThreshold=0.5, task="det"):
+        self.detector = LineSqueezeDetect(det_model_path, confThreshold, nmsThreshold, task)
+        self.ocr = TextRecognition(model_dir=ocr_model_path, model_name='en_PP-OCRv5_mobile_rec')
+
+    def infer(self, image: np.ndarray) -> OCRResult:
+        # DetectResult
+        results = self.detector.infer(image)
+
+        rois = [image[int(box[1]) + 10 : int(box[3]) - 10, int(box[0]) : int(box[2])] for box in results.boxes]
+
+        res = [res['rec_text'] for res in self.ocr.predict(input=rois) if len(res['rec_text']) > 2]
+        return OCRResult(text=res, boxes=results.boxes, class_ids=results.class_ids)
+
