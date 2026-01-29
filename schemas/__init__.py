c9ac5ae5b7dbbdf3829e6aea364fd46966fad699
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-29 05:27:44 +0000

    dev: feat: 新增LineSqueezeRequest类

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 07:51:34
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-27 10:20:08
+@LastEditTime : 2026-01-28 07:18:18
 @FilePath     : __init__.py
 @Description  :
 '''
@@ -10,3 +10,4 @@
 from .dc_fuse_schemas import DCFuseRequest
 from .common import CommonResponse, EmptyRequest
 from .indicator_schemas import IndicatorRequest
+from .lineSqueeze_schemas import LineSqueezeRequest
---
+++ schemas/lineSqueeze_schemas.py
@@ -0,0 +1,14 @@
+'''
+@Author       : gongzhang4
+@Date         : 2026-01-28 07:17:01
+@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-01-28 07:17:02
+@FilePath     : lineSqueeze_schemas.py
+@Description  :
+'''
+
+from .dc_fuse_schemas import DCFuseRequest
+
+
+class LineSqueezeRequest(DCFuseRequest):
+    pass
