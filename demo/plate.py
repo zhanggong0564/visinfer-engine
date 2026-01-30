1f05ca8885ac2da8176e76b5ce8aec65477f85bc
zhanggong1 <zhanggong1@sungrowpower.com>
2026-01-30 09:35:53 +0000

    dev: refactor: Refactored model initialization and detection process using detection_factory and InputParamsBusiness

--- demo/plate.py
+++ demo/plate.py
@@ -2,18 +2,23 @@
 @Author       : gongzhang4
 @Date         : 2026-01-13 05:15:10
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-13 06:50:27
+@LastEditTime : 2026-01-29 12:44:24
 @FilePath     : plate.py
 @Description  :
 '''
 
-from services import PlateScrewJudgeApi
 import cv2
+from services import detection_factory
+from services.data_base import InputParamsBusiness
 
 if __name__ == '__main__':
-    model_path = "/data/zhanggong/workspace/project/move_vsion/mobile_vision/weights/mobile_vision_plate_v2.onnx"
     image_path = "/data/zhanggong/workspace/project/move_vsion/mobile_vision/demo/data/plate/IMG_7141.JPG"
     image = cv2.imread(image_path)
-    confThreshold = 0.5
-    infer = PlateScrewJudgeApi(model_path, confThreshold)
-    print(infer.detect(image))
+    # confThreshold = 0.5
+    # model = plate_screw_detect(settings.plate_screw.model_path, settings.plate_screw.confThreshold)
+    # print(model.infer(image))
+    model = detection_factory.get_scenarios("plate_screw")
+    inputs = InputParamsBusiness(image=image)
+    print(model.detect(inputs))
+    # infer = PlateScrewJudgeApi(model_path, confThreshold)
+    # print(infer.detect(image))
