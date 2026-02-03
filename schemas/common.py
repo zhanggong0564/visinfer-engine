98ded72dcda439625b0c48689fa858f0059f3296
zhanggong1 <zhanggong1@sungrowpower.com>
2026-02-03 03:42:19 +0000

    dev: fix: 提取关键代码改动： 将DetailListItem模型替换为DetectionItem模型

--- schemas/common.py
+++ schemas/common.py
@@ -2,13 +2,14 @@
 @Author       : gongzhang4
 @Date         : 2026-01-07 07:48:54
 @LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-01-27 10:18:52
+@LastEditTime : 2026-02-03 03:40:29
 @FilePath     : common.py
 @Description  :
 '''
 
 from pydantic import BaseModel, Field
 from typing import List, Literal
+from services.data_base import DetectionItem
 
 
 class GuideLineItem(BaseModel):
@@ -25,19 +26,19 @@ class ExampleImageItem(BaseModel):
     FilePath: str = Field(..., description="示例图片访问路径")
 
 
-class DetailListItem(BaseModel):
-    """result中detailList的单个元素模型(精准匹配示例）"""
+# class DetailListItem(BaseModel):
+#     """result中detailList的单个元素模型(精准匹配示例）"""
 
-    status: bool = Field(..., description="检测状态(false=正常/未检出异常,true=异常）")
-    scene: str = Field(..., description="检测类别(screw/metal_piece/brass_plate等)")
-    coordinate: List[float] = Field(..., description="检测轮廓点坐标顺序连接")
-    accuracy: float = Field(..., description="检测置信度(0-1之间)")
+#     status: bool = Field(..., description="检测状态(false=正常/未检出异常,true=异常）")
+#     scene: str = Field(..., description="检测类别(screw/metal_piece/brass_plate等)")
+#     coordinate: List[float] = Field(..., description="检测轮廓点坐标顺序连接")
+#     accuracy: float = Field(..., description="检测置信度(0-1之间)")
 
 
 class ResultResponse(BaseModel):
     """顶层返回的result对象模型"""
 
-    detailList: List[DetailListItem] = Field(..., description="检测详情列表")
+    detailList: List[DetectionItem] = Field(..., description="检测详情列表")
     status: Literal["true", "false"] = Field(..., description="整体检测状态（false=正常，true=异常）")
     error_msg: str = Field(..., description="错误信息（无错误则为空字符串）")
     message: str = Field(..., description="检测结果描述（如“检测成功”）")
