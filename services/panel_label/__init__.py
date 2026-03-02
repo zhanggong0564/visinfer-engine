4b30293a497120fe79682fe9d522ed48cd392436
张弓 <zhanggong1@sungrowpower.com>
2026-03-02 07:59:44 +0000

    panel_label_ocr: feat: 修改文件导入 从PanelLabelDetect导入为OCRPipeline和PanelLabelJudgeApi

--- services/panel_label/__init__.py
+++ services/panel_label/__init__.py
@@ -2,9 +2,10 @@
 @Author       : gongzhang4
 @Date         : 2026-02-26 09:54:00
 @LastEditors  : 张弓 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-02-26 09:54:01
+@LastEditTime : 2026-03-02 07:57:56
 @FilePath     : __init__.py
 @Description  :
 '''
 
-from .panel_label_detect import PanelLabelDetect
+from .panel_label_detect import OCRPipeline
+from .business_logic import PanelLabelJudgeApi
