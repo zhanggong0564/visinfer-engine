bd35fdf93ad78c2fd18344bfd60bbcedb0613376
张弓 <zhanggong1@sungrowpower.com>
2026-03-27 10:14:55 +0000

    panel_label_ocr: refactor: 修改置信度阈值和NMS阈值

--- config/panel_label_config.py
+++ config/panel_label_config.py
@@ -2,7 +2,7 @@
 @Author       : gongzhang4
 @Date         : 2026-03-02 08:01:03
 @LastEditors  : 张弓 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-03-02 08:02:05
+@LastEditTime : 2026-03-25 02:15:05
 @FilePath     : panel_label_config.py
 @Description  :
 '''
@@ -11,5 +11,5 @@
 class PanelLabelConfig:
     model_path = "./weights/panel_label/best_v1.onnx"
     orient_model_path = "./weights/panel_label/PP-LCNet_x1_0_textline_ori"
-    confThreshold = 0.4
-    nmsThreshold = 0.5
+    confThreshold = 0.7
+    nmsThreshold = 0.8
