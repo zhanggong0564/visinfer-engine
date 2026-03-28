8caf0297111f17ac23fc65f7f2aa59aad3e5ea01
张弓 <zhanggong1@sungrowpower.com>
2026-03-28 05:30:37 +0000

    panel_label_ocr: fix: 关键改动：将PlateScrewJudgeApi替换为PanelLabelJudgeApi

--- services/__init__.py
+++ services/__init__.py
@@ -1,15 +1,15 @@
 '''
 @Author       : gongzhang4
 @Date         : 2026-01-07 06:11:10
-@LastEditors  : zhanggong1 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-02-07 08:21:38
+@LastEditors  : 张弓 zhanggong1@sungrowpower.com
+@LastEditTime : 2026-03-27 12:21:45
 @FilePath     : __init__.py
 @Description  :
 '''
 
-from .plate_screw import PlateScrewJudgeApi
+from .panel_label import PanelLabelJudgeApi
 from .utils import rotate_points
 from .api import detection_factory
 
 
-__all__ = ["PlateScrewJudgeApi", "rotate_points", "detection_factory"]
+__all__ = ["PanelLabelJudgeApi", "rotate_points", "detection_factory"]
