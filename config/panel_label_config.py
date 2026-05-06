fb68b5f9d6e0fc1a0227147c12c79d71d6628978
张弓 <zhanggong1@sungrowpower.com>
2026-05-06 07:18:15 +0000

    panel_label_ocr: refactor: 模型更新v2

--- config/panel_label_config.py
+++ config/panel_label_config.py
@@ -2,15 +2,15 @@
 @Author       : gongzhang4
 @Date         : 2026-03-02 08:01:03
 @LastEditors  : 张弓 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-03-28 05:37:35
+@LastEditTime : 2026-04-22 09:50:46
 @FilePath     : panel_label_config.py
 @Description  :
 '''
 
 
 class PanelLabelConfig:
-    model_path = "./weights/panel_label/best_v1.onnx"
-    orient_model_path = "./weights/panel_label/PP-LCNet_x1_0_textline_ori"
-    text_recognition_model_path = "./weights/panel_label/PP-OCRv5_server_rec_plane_infer"
-    confThreshold = 0.7
+    model_path = "./weights/panel_label/best_v2.onnx"
+    orient_model_path = "./weights/panel_label/PP-LCNet_x1_0_textline_ori_v2"
+    text_recognition_model_path = "./weights/panel_label/PP-OCRv5_server_rec_plane_infer_v2"
+    confThreshold = 0.72
     nmsThreshold = 0.8
