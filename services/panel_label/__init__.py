7e92dc671c136c0c8b205fad6aabcdf61e510177
张弓 <zhanggong1@sungrowpower.com>
2026-05-14 01:51:32 +0000

    panel_label_ocr: feat: 提取关键代码改动： 添加了新模块导入 PRODUCT_TYPE 和 PRODUCT_guideline

--- services/panel_label/__init__.py
+++ services/panel_label/__init__.py
@@ -2,10 +2,11 @@
 @Author       : gongzhang4
 @Date         : 2026-02-26 09:54:00
 @LastEditors  : 张弓 zhanggong1@sungrowpower.com
-@LastEditTime : 2026-03-24 07:27:59
+@LastEditTime : 2026-04-21 02:24:52
 @FilePath     : __init__.py
 @Description  :
 '''
 
 from .panel_label_detect import OCRPipeline, OCRPipelineCrop
 from .business_logic import PanelLabelJudgeApi
+from .product_type import PRODUCT_TYPE, PRODUCT_guideline
