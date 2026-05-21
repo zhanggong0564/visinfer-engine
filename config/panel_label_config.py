510d7981bc38fb65ddfa8cf20596014c06ed9846
zhanggong <zhanggong1@@sungrowpower.com>
2026-05-21 11:54:30 +0800

    config: update: 新增 OCR 文本检测相关配置参数
    - 新增 text_det_limit_side_len、text_det_limit_type 等检测配置
    - 新增 text_rec_score_thresh、text_rec_input_shape 识别配置

--- config/panel_label_config.py
+++ config/panel_label_config.py
@@ -14,3 +14,12 @@ class PanelLabelConfig:
     text_recognition_model_path = "./weights/panel_label/PP-OCRv5_server_rec_plane_infer_v3"
     confThreshold = 0.75
     nmsThreshold = 0.8
+    # TextDetection
+    text_det_limit_side_len = 480
+    text_det_limit_type = "max"
+    text_det_thresh = 0.3
+    text_det_box_thresh = 0.4
+    text_det_unclip_ratio = 2.0
+    # TextRecognition
+    text_rec_score_thresh = 0.7
+    text_rec_input_shape = [3, 48, 320]
